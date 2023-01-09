8d398eec8176203e090b5119c03354650f91b886
Taiki-nk <taikifate.0206@gmail.com>
2023-01-09 15:19:41 +0900

    :tada:[feat] 簡易チャット完成

--- index.js
+++ index.js
@@ -2,11 +2,21 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello world</h1>");
+  res.sendFile(__dirname + "/index.html");
 });
 
-server.listen(3000, () => {
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+	io.emit('chat message',msg)
+  });
+});
+
+server.listen(PORT, () => {
   console.log("listening on *:3000");
 });
